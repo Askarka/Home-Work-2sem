d55da36bb25ba750c6accb32431c2fb361842aec
Askar <askarkaka@icloud.com>
2020-03-26 02:23:40 +0300

    Found mistake with Collections names in fourth programming home work, added new home work packages.

--- src/HomeWork/prog/_4/ModifiableCollection.java
+++ src/HomeWork/prog/_4/ModifiableCollection.java
@@ -13,10 +13,10 @@ public class ModifiableCollection<T> extends AbstractCollection<T> {
     }
 
     public ModifiableCollection(ModifiableCollection<? extends T> c) {
-
         int i = 0;
         this.array = (T[]) new Object[c.size];
         Iterator<T> it = (Iterator<T>) c.iterator();
+
         while(it.hasNext()) {
             array[i] = it.next();
             i++;
@@ -25,7 +25,7 @@ public class ModifiableCollection<T> extends AbstractCollection<T> {
     }
 
     @Override
-    public Iterator<T> iterator()  {
+    public Iterator<T> iterator() {
         return new UniversalEndlessArrayIterator<T>(array);
     }
 
@@ -33,4 +33,35 @@ public class ModifiableCollection<T> extends AbstractCollection<T> {
     public int size() {
         return this.size;
     }
+
+    public boolean equals(ModifiableCollection<T> o) {
+        if(this.size != o.size){
+            return false;
+        }
+
+        int n = 0;
+        for(int i = 0; i < this.size; i++){
+            for(int j = 0; j < this.size; j++){
+                if(this.array[i] == o.array[j]){
+                  n++;
+                }
+            }
+        }
+
+        if(this.size == n){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean add(T o){
+        try{
+            array[size++] = o;
+            return true;
+        }catch (IndexOutOfBoundsException a){
+            return false;
+        }
+    }
+
 }
