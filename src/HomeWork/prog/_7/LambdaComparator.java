8edb09175ee7f99a0ca5a4f99baad586bbff989c
askar <askarkaka@icloud.com>
2020-04-25 21:55:52 +0300

    deleted semester work

---
+++ src/HomeWork/prog/_7/LambdaComparator.java
@@ -0,0 +1,10 @@
+package HomeWork.prog._7;
+
+import java.util.Comparator;
+
+public class LambdaComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+}
